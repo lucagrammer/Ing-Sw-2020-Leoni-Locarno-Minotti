8998cf490823ec17ea215847a6ec6690d7c7ae59
Luca Leoni <luca3.leoni@mail.polimi.it>
2020-03-31 13:08:50 +0200

    Add new classes: Game and Player

---
+++ src/main/java/Model/Game.java
@@ -0,0 +1,91 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Game {
+    private int numPlayer;
+    private List<Player> players;
+    private Board board;
+    private int gameId;
+    private ArrayList<Card> usedCards;
+
+    /**
+     * Build the Game
+     * @param player    the player who creates the game
+     * @param numPlayer the number of players
+     */
+    public Game(Player player, int numPlayer){
+        this.players = new ArrayList<Player>();
+        this.players.add(player);
+        this.numPlayer=numPlayer;
+        this.usedCards = new ArrayList<Card>();
+        this.board= new Board(this);
+    }
+
+    /**
+     * Gets the board of the game
+     * @return the board of the game
+     */
+    public Board getBoard() {
+        return board;
+    }
+
+    /**
+     * Add the player to the game
+     * @param player the new player
+     */
+    public void addPlayer(Player player){
+        if(players.size() < numPlayer) {
+            players.add(player);
+            players = players.stream().sorted((o1, o2) -> o2.getDateOfBirth().compareTo(o1.getDateOfBirth())).collect(Collectors.toList());
+        }
+    }
+
+    /**
+     * Gets all the players of the game
+     * @return all the players of the game
+     */
+    public List<Player> getPlayers() {
+        return new ArrayList<>(players);
+    }
+
+    /**
+     * Sets all the cards that will be used during the game
+     * @param cards the cards selected
+     */
+    public void setUsedCards(List<Card> cards) {
+        usedCards.clear();
+        usedCards.addAll(cards);
+    }
+
+    /**
+     * Gets all the cards that will be used during the game
+     * @return the cards
+     */
+    public List<Card> getUsedCards(){
+        return new ArrayList<>(usedCards);
+    }
+
+    /**
+     * Check if everything is ready to start the game
+     * @return true if all the players has joined the game and the cards has been set
+     */
+    public boolean isReady(){
+        return players.size() == numPlayer && usedCards.size() == numPlayer;
+    }
+
+    /**
+     * Gets the youngest player who is older than a specified player
+     * @param player the specified player
+     * @return the youngest player who is older than the specified player
+     */
+    public Player getNextPlayer(Player player){
+        if(player == null || !players.contains(player) || players.indexOf(player) == players.size() -1)
+            return players.get(0);
+        else
+            return players.get(players.indexOf(player) +1);
+    }
+
+}
---
+++ src/main/java/Model/Player.java
@@ -0,0 +1,89 @@
+package Model;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Player {
+    private String nickname;
+    private Date dateOfBirth;
+    private Worker maleWorker;
+    private Worker femaleWorker;
+    private Card card;
+
+    /**
+     * Constructor: build a player
+     * @param nickname    Player's nickname
+     * @param dateOfBirth Player's date of birth
+     */
+    public Player(String nickname, Date dateOfBirth){
+        this.nickname = nickname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * Gets the nickname of the player
+     * @return Player's nickname
+     */
+    public String getNickname(){
+        return nickname;
+    }
+
+    /**
+     * Gets the date of birth of the player
+     * @return Player's date of birth
+     */
+    public Date getDateOfBirth(){
+        return dateOfBirth;
+    }
+
+    /**
+     * The player chooses his color
+     * @param color The color chosen by the player
+     */
+    public void chooseColor(Color color){
+        this.maleWorker = new Worker(color, Genre.MALE, this);
+        this.femaleWorker = new Worker(color, Genre.FEMALE, this);
+    }
+
+    /**
+     * Get the male worker
+     * @return the male worker
+     */
+    public Worker getMaleWorker() {
+        return maleWorker;
+    }
+
+    /**
+     * Get the female worker
+     * @return the female worker
+     */
+    public Worker getFemaleWorker() {
+        return femaleWorker;
+    }
+
+    /**
+     * The player chooses his card
+     * @param card The card chosen by the player
+     */
+    public void setCard(Card card){
+        this.card = card;
+    }
+
+    /**
+     * Get the card
+     * @return the card
+     */
+    public Card getCard() {
+        return card;
+    }
+
+    /**
+     * Compares the nicknames of two players
+     * @param player The second player
+     * @return true if the players match
+     */
+    public boolean equals(Player player){
+        return this.nickname.equals(player.nickname);
+    }
+
+}
